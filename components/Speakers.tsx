a665ca9940be52e1db4f0d7505f0fe7dd175fcfe
hamiltonmatematica <hamilton.vinicius@gmail.com>
2026-01-21 13:16:35 -0300

    Adjust Fred's photo to center face

--- components/Speakers.tsx
+++ components/Speakers.tsx
@@ -25,7 +25,7 @@ export const Speakers: React.FC = () => {
                     alt={speaker.name}
                     className="w-full h-full object-cover transition-all duration-500"
                     style={{
-                      transform: speaker.name === 'Fred Rocha' ? 'scale(1.6) translateY(15px)' : 'none'
+                      transform: speaker.name === 'Fred Rocha' ? 'scale(1.8) translateY(-35px)' : 'none'
                     }}
                   />
                 </div>
